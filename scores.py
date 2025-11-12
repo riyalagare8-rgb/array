5f86f84ce2eb7519a4b9b9e5e73b0681f0a8ddb7
riyalagare8-rgb <riyalagare8@gmail.com>
2025-11-12 12:56:15 +0530

    Update predefined scores and calculate average

--- scores.py
+++ scores.py
@@ -1,13 +1,13 @@
 import sys
 
-# Another set of predefined scores
-scores = [72, 88, 95, 63, 80, 77]
 
-# Calculate total and average
+scores = [85,90,78,92]
+
+
 total = sum(scores)
 average = total / len(scores)
 
-# Output results
+
 print("Scores:", scores)
 print("Total Score:", total)
 print("Average Score:", average)
